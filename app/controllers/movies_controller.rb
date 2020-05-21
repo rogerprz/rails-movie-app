b4c6e2dd692e7a599c7864c461c5e2829ca609df
rogerprz <rogerprz@github.com>
2020-05-21 10:57:55 -0700

    update index

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,8 +1,18 @@
 class MoviesController < ApplicationController
 
-  def index
-    @movies = Movie.all
+  def new
+    @movie = Movie.new
   end
+  def index
+    movies = Movie.all
+
+    render :index, locals: { movies: movies }
+  end
+
+  def edit
+    @movie = Movie.find(params[:id])
+  end
+
   def show
     @movie = Movie.find(params[:id])
   end
--- test/system/movies_system_test.rb
+++ test/system/movies_system_test.rb
@@ -9,4 +9,14 @@ class MoviesSystemTest < ApplicationSystemTestCase
     assert_text "Parasite"
     assert_text "Bong Joon-ha"
   end
+
+  test "vist the index page"do
+    movie1 = Movie.create(title: "The Breakfast Club", director: "John Hughes")
+    movie2 = Movie.create(title: "Bridesmaids", director: "John Hughes")
+
+    visit "/movies"
+
+    assert_text = "The Breakfast Club"
+    assert_text = "Bridesmaids"
+  end
 end
